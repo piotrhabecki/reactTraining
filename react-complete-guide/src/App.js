988dc6b1d0ace8e853d41f2858896d1d34d3239d
piotr.habecki@jeppesen.com <piotr.habecki@jeppesen.com>
2020-02-27 17:16:18 +0100

    along with the course.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,16 +7,17 @@ class App extends Component {
   state = {
     persons: [
       { name: 'Piotr', job: 'tester' },
-      { name: 'Pawel', job: 'developer'}
-    ]
+      { name: 'Pawel', job: 'developer' }
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
     console.log('Was clicked!');
-    this.setState( {
+    this.setState({
       persons: [
         { name: newName, job: 'tester developer' },
-        { name: 'Pawel', job: 'developer'}
+        { name: 'Pawel', job: 'developer' }
       ]
     })
   }
@@ -24,43 +25,59 @@ class App extends Component {
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
-        { name: event.target.value , job: 'tester developer' },
-        { name: 'Pawel', job: 'developer'}
+        { name: event.target.value, job: 'tester developer' },
+        { name: 'Pawel', job: 'developer' }
       ]
     })
   }
 
+  togglePersonsHandler = () =>{
+
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  whatToWrite = () => {
+    let returnedString = "";
+    this.state.showPersons ? returnedString = "hide" : returnedString = "show";
+    return returnedString; 
+  }
+
   render() {
-  
+
     const style = {
-    backgroundColor: 'white',
-    font: 'inherti',
-    border: '1x solid blue',
-    padding: '8px',
-    cursor: 'pointer'
-  };
+      backgroundColor: 'white',
+      font: 'inherti',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
 
         <h1>I'm react page</ h1>
 
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler('Piotrek')}>Switch name</button>
-        
-        <Person 
-          name={this.state.persons[0].name} 
-          job={this.state.persons[0].job}
-          click={this.switchNameHandler.bind(this, 'Karolina')}
-          changed={this.nameChangedHandler}
-        />
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>{this.whatToWrite()} persons</button>
 
-        <Person 
-          name={this.state.persons[1].name} 
-          job={this.state.persons[1].job}
-          click={this.switchNameHandler}> My task is: C2E
-        </ Person>
+        { this.state.showPersons ? 
+          <div>
+          <Person
+            name={this.state.persons[0].name}
+            job={this.state.persons[0].job}
+            click={this.switchNameHandler.bind(this, 'Karolina')}
+            changed={this.nameChangedHandler}
+          />
 
+          <Person
+            name={this.state.persons[1].name}
+            job={this.state.persons[1].job}
+            click={this.switchNameHandler}> My task is: C2E
+          </ Person>
+
+        </div> : null
+        }
       </div>
     );
   }
