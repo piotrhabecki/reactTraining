1a92869533ecac0556cdc8a778fe1d6eb53c21e8
piotr.habecki@jeppesen.com <piotr.habecki@jeppesen.com>
2020-05-22 11:05:05 +0200

    Assigment 3 done

--- react-complete-guide-assigment-2/src/App.js
+++ react-complete-guide-assigment-2/src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Validation} from './validation'
+import {Char} from './char'
 
 class App extends Component {
 
-  wordDisplayListener = () => {
-    
+  state = {
+    userInput: ''
+  }
+
+  inputChangeHandler = (event) => {
+      this.setState({userInput: event.target.value});
+  }
+  
+  deleteChartHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
   }
 
   render() {
+
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+      character={ch}
+      key={index}
+      clicked={() => this.deleteChartHandler(index)}/>;
+    });
+
     return (
       <div className="App">
         <p>
-        <input type="text" onChange={this.wordDisplayListener()}/>
+        <input 
+        type="text" 
+        onChange={this.inputChangeHandler} 
+        value={this.state.userInput} />
         </p>
+        <p>
+          {this.state.userInput}
+        </p>
+        <Validation inputLength={this.state.userInput.length}/>
+        {charList}
       </div>
     );
   }
